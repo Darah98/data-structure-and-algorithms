cc3a0b1261a9af996afd07faf14574f0e65da5f2
darah98 <darashalabi@gmail.com>
2020-05-30 03:21:41 +0300

    cc05 done

--- code-challenges/LinkedList/linked-list.js
+++ code-challenges/LinkedList/linked-list.js
@@ -1,3 +1,52 @@
-function(){
-    
+'use strict';
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insert(value) {
+    try {
+      const node = new Node(value);
+      node.next = this.head;
+      this.head = node;
+    } catch (err) {
+      console.error(error);
+    }
+  }
+  includes(value) {
+    try {
+      let currentValue = this.head;
+      while (currentValue) {
+        if (currentValue.value === value) {
+          return true;
+        }
+        currentValue = currentValue.next;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  toString() {
+    try {
+      let currentValue = this.head;
+      while (currentValue) {
+        console.log(`{${currentValue.value}} -> `);
+        currentValue = currentValue.next;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+const list = new LinkedList();
+list.insert('c');
+list.insert('b');
+list.insert('a');
+list.includes('b');
+list.toString();
+console.log(list);
