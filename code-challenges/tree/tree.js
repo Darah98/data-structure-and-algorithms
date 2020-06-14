900e04dfbce7288c7937710c5bf62f943f9e5a24
Darah <darashalabi@gmail.com>
2020-06-14 19:17:37 +0300

    add fixed

--- code-challenges/tree/tree.js
+++ code-challenges/tree/tree.js
@@ -1,57 +1,63 @@
-class Node{
-    constructor(value, left=null, right=null){
-        this.value= value;
-        this.left= left;
-        this.right= right;
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 }
-class BinaryTree{
-    constructor(root){
-        this.root= root;
+class BinaryTree {
+    constructor(root) {
+        this.root = root;
     }
-    preOrder(){
-        const result= [];
-        const traverseThru= (node) =>{
+    preOrder() {
+        const result = [];
+        const traverseThru = (node) => {
             result.push(node.value);
-            if(node.left) traverseThru(node.left);
-            if(node.right) traverseThru(node.right);
+            if (node.left) traverseThru(node.left);
+            if (node.right) traverseThru(node.right);
         }
         traverseThru(this.root);
         return result;
     }
-    inOrder(){
-        const result= [];
-        const traverseThru= (node)=>{
-            if(node.left) traverseThru(node.left);
+    inOrder() {
+        const result = [];
+        const traverseThru = (node) => {
+            if (node.left) traverseThru(node.left);
             result.push(node.value);
-            if(node.right) traverseThru(node.right);
+            if (node.right) traverseThru(node.right);
         }
         traverseThru(this.root);
         return result;
     }
-    postOrder(){
-        const result= [];
-        const traverseThru= (node)=>{
-            if(node.left) traverseThru(node.left);
-            if(node.right) traverseThru(node.right);
+    postOrder() {
+        const result = [];
+        const traverseThru = (node) => {
+            if (node.left) traverseThru(node.left);
+            if (node.right) traverseThru(node.right);
             result.push(node.value);
         }
         traverseThru(this.root);
         return result;
     }
 }
-class BinarySearchTree{
-    constructor(){}
-    add(value){
-        const node= new Node(value, left, right);
-        const tree= new BinaryTree();
-        const result= tree.preOrder();
-        result.push(node);
+class BinarySearchTree {
+    constructor() { }
+    add(value) {
+        const node = new Node(value);
+        const tree= new BinaryTree(node)
+        if (!tree.root) {
+            tree.root = node;
+        }
+        let current = tree.root;
+        while (current.left || current.right) {
+            current = current.left ? current.left : current.right;
+        }
+        current.left = node;
     }
-    contains(value){
-        const tree= new BinaryTree();
-        const result= tree.preOrder();
-        if(result.includes(value)) true;
+    contains(value) {
+        const tree = new BinaryTree();
+        const result = tree.preOrder();
+        if (result.includes(value)) true;
         false;
     }
 }
