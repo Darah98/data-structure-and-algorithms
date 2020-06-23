0d1592296b733ac6791ae85a35aa100b4f3db609
Darah <darashalabi@gmail.com>
2020-06-23 23:18:35 +0300

    fixed

--- code-challenges/fizzbuzztree/fizzbuzz-tree.js
+++ code-challenges/fizzbuzztree/fizzbuzz-tree.js
@@ -1,58 +1,59 @@
+/* eslint-disable eqeqeq */
 
 class Node {
-    constructor(value, left = null, right = null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
 }
 class BinaryTree {
-    constructor(root) {
-        this.root = root;
+  constructor(root) {
+    this.root = root;
+  }
+  preOrder() {
+    const result = [];
+    const traverseThru = (node) => {
+      result.push(node.value);
+      if (node.left) traverseThru(node.left);
+      if (node.right) traverseThru(node.right);
     }
-    preOrder() {
-        const result = [];
-        const traverseThru = (node) => {
-            result.push(node.value);
-            if (node.left) traverseThru(node.left);
-            if (node.right) traverseThru(node.right);
-        }
-        traverseThru(this.root);
-        return result;
+    traverseThru(this.root);
+    return result;
+  }
+  inOrder() {
+    const result = [];
+    const traverseThru = (node) => {
+      if (node.left) traverseThru(node.left);
+      result.push(node.value);
+      if (node.right) traverseThru(node.right);
     }
-    inOrder() {
-        const result = [];
-        const traverseThru = (node) => {
-            if (node.left) traverseThru(node.left);
-            result.push(node.value);
-            if (node.right) traverseThru(node.right);
-        }
-        traverseThru(this.root);
-        return result;
+    traverseThru(this.root);
+    return result;
+  }
+  postOrder() {
+    const result = [];
+    const traverseThru = (node) => {
+      if (node.left) traverseThru(node.left);
+      if (node.right) traverseThru(node.right);
+      result.push(node.value);
     }
-    postOrder() {
-        const result = [];
-        const traverseThru = (node) => {
-            if (node.left) traverseThru(node.left);
-            if (node.right) traverseThru(node.right);
-            result.push(node.value);
-        }
-        traverseThru(this.root);
-        return result;
+    traverseThru(this.root);
+    return result;
+  }
+  add(value) {
+    const node = new Node(value);
+    if (!this.root) {
+      this.root = node;
     }
-    add(value) {
-        const node = new Node(value);
-        if (!this.root) {
-            this.root = node;
-        }
-        let current = this.root;
-        while (current.left || current.right) {
-            current = current.left ? current.left : current.right;
-        }
-        current.left ? current.right = node : current.left = node;
-        // const result= tree.preOrder();
-        // result.push(node);
+    let current = this.root;
+    while (current.left || current.right) {
+      current = current.left ? current.left : current.right;
     }
+    current.left ? current.right = node : current.left = node;
+    // const result= tree.preOrder();
+    // result.push(node);
+  }
 }
 const node = new Node(5);
 const newtree = new BinaryTree(node);
@@ -63,24 +64,19 @@ console.log(newtree)
 
 
 function fizzBuzzTree(tree) {
-    let newArr = [];
-    let current = tree.root;
-    while (current.left || current.right) {
-        newArr.push(current.value);
-        current = current.left ? current.left : current.right;
+  let current = tree.root;
+  while (current.left || current.right) {
+    if (current.value % 5 == 0 && current.value % 3 == 0) {
+      current.value = 'FizzBuzz';
+    } else if (current.value % 5 == 0) {
+      current.value = 'Buzz';
+    } else if (current.value % 3 == 0) {
+      current.value = 'Fizz';
+    } else {
+      current.value = `${current.value}`;
     }
-    newArr.push(current.value);
-    for (let i = 0; i < newArr.length; i++) {
-        if (newArr[i] % 5 == 0 && newArr[i] % 3 == 0) {
-            newArr[i] = 'FizzBuzz';
-        } else if (newArr[i] % 5 == 0) {
-            newArr[i] = 'Buzz';
-        } else if (newArr[i] % 3 == 0) {
-            newArr[i] = 'Fizz';
-        } else {
-            newArr[i] = `${newArr[i]}`;
-        }
-    }
-    return newArr;
+    current = current.left ? current.left : current.right;
+  }
+  return tree;
 }
 fizzBuzzTree(newtree);
